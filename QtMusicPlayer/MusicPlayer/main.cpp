7c0099c0d9ec91bd2508e8e4ae26f2483fc79b77
wmz <wmz@gmail.com>
2022-11-08 23:25:47 +0800

    mp3

--- QtMusicPlayer/MusicPlayer/main.cpp
+++ QtMusicPlayer/MusicPlayer/main.cpp
@@ -1,10 +1,23 @@
 #include "MusicPlayer.h"
 #include <QtWidgets/QApplication>
-
-int main(int argc, char *argv[])
+#include <QApplication>
+#include"connection.h"
+#include<QTime>
+#include<QSplashScreen>
+#include<QPixmap>
+int main(int argc, char* argv[])
 {
+
+    qsrand(QTime(0, 0, 0).secsTo(QTime::currentTime()));
+
     QApplication a(argc, argv);
+    if (!CreatConnection())
+    {
+        return 1;
+    }
     MusicPlayer w;
+    w.setWindowIcon(QIcon(u8"ooopic_1520340986.ico"));
+    w.setWindowTitle((u8"Œ“µƒ“Ù¿÷"));
     w.show();
     return a.exec();
 }
